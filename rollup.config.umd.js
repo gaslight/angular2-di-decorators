84d1d1ba7b7ca2f09f62337228e1498225eee988
Chris Nelson <superchrisnelson@gmail.com>
2016-03-12 21:11:42 -0500

    working npm

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -2,6 +2,6 @@ import config from './rollup.config';
 
 config.format = 'umd';
 config.dest = 'dist/angular2-di-decorators.umd.js';
-config.moduleName = 'angular2-di-decorators';
+config.moduleName = 'angular2_di_decorators';
 
 export default config;
